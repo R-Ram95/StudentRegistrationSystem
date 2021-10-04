7ee33ae113f54a06189e5fe788f43b4f39159408
Rohinesh Ram <rohinesh@ualberta.ca>
2021-10-04 00:27:26 -0600

    Removed comments from main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-//        LoginPage login = new LoginPage();
-//        login.displayGUI();
 
         RegistrationApp app = new RegistrationApp();
         app.displayApp();
-
-//        StudentCoursesPage student = new StudentCoursesPage();
-//        student.displayGUI();
     }
 }
