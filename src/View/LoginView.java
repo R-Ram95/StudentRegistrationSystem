e6503e451459fea0f8b6b7de2c5aabf8b3bdce0c
Emily Wang <emilywang@Emilys-MBP.hitronhub.home>
2021-10-06 15:27:36 -0600

    updated the view for login

--- src/View/LoginView.java
+++ src/View/LoginView.java
@@ -12,26 +12,41 @@ import java.awt.event.ActionListener;
  */
 public class LoginView extends JFrame{
     JButton loginButton = new JButton("Login");
-    JTextField nameText = new JTextField(10);
-    JTextField studentIdText = new JTextField(10);
+    JTextField nameText = new JTextField();
+    JTextField studentIdText = new JTextField();
 
     public void displayGUI(){
-        JFrame frame = new JFrame("Registration Page");
-        frame.setSize(600, 200);
-        // registration panel
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(3,2));
+        JFrame frame = new JFrame();
 
-        JLabel nameLabel = new JLabel("Student Name: ");
+        //configure the frame
+        frame.setSize(350, 200);
+        frame.add(panel);
+
+        //Set the layout
+        panel.setLayout(null);
+
+        //name label and textbox
+        JLabel nameLabel = new JLabel("Name: ");
+        nameLabel.setBounds(10, 20, 80, 25);
         panel.add(nameLabel);
+
+
+        nameText.setBounds(150, 20, 165, 25);
         panel.add(nameText);
-        JLabel studentIdLabel = new JLabel("Student Number: ");
-        panel.add(studentIdLabel);
+
+        //student number label and textbox
+        JLabel stuNumLabel = new JLabel("Student Number: ");
+        stuNumLabel.setBounds(10, 50, 120, 25);
+        panel.add(stuNumLabel);
+
+        studentIdText.setBounds(150, 50, 165, 25);
         panel.add(studentIdText);
+
+        loginButton.setBounds(100, 90, 100, 25);
         panel.add(loginButton);
 
-        // add registration view
-        frame.add(panel);
+
         frame.setVisible(true);
     }
 
